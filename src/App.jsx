735d6a3cf9a453889afd94262a6ace2a4b2a7fe0
Juan <Jjangulo91@gmail.com>
2025-02-07 12:21:58 +0100

    otro comit del viernes a las 12:21

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import "./App.css";
+import { useState } from "react";
 function App() {
   const [showModal, setShowModal] = useState(false);
   const openModal = () => {
@@ -8,34 +8,38 @@ function App() {
   const closeModal = () => {
     setShowModal(false);
   };
-
   return (
     <>
-      <button className="show-modal">Show modal 1</button>
-      <button className="show-modal">Show modal 2</button>
-      <button className="show-modal">Show modal 3</button>
-       {showModal && (
-<
-
->
-
-       )}
-      <div className="modal">
-        <button className="close-modal">&times;</button>
-        <h1>I'm a modal window 😍</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-      <div className="overlay"></div>
+      <button className="show-modal" onClick={openModal}>
+        Show modal 1
+      </button>
+      <button className="show-modal" onClick={openModal}>
+        Show modal 2
+      </button>
+      <button className="show-modal" onClick={openModal}>
+        Show modal 3
+      </button>
+      {showModal && (
+        <>
+          <div className="modal">
+            <button className="close-modal" onClick={closeModal}>
+              &times;
+            </button>
+            <h1>I&apos;m a modal window :ojos_de_corazón:</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className="overlay" onClick={closeModal}></div>
+        </>
+      )}
     </>
   );
 }
-
 export default App;
